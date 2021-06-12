07b38eece59d9e5176d1618562a57c1023bd25c3
Xemur0 <77635277+Xemur0@users.noreply.github.com>
2021-06-12 18:14:42 +0300

    lesson_5

---
+++ les5.sql
@@ -0,0 +1,51 @@
+use shop;
+
+SELECT * FROM products;
+
+    SET created_at = NULL where id = 3;
+
+UPDATE products
+    SET created_at = NULL where id = 1;
+
+UPDATE products
+    SET created_at = NOW() where created_at is NULL;
+
+UPDATE products
+    SET created_at = NOW() where id = 3;
+
+ALTER TABLE products 
+    CHANGE COLUMN `created_at` `created_at` VARCHAR(256) NULL,
+    CHANGE COLUMN `updated_at` `updated_at` VARCHAR(256) NULL;
+
+describe products;
+SELECT created_at from products;
+
+ALTER TABLE products 
+    CHANGE COLUMN `created_at` `created_at` DATETIME DEFAULT CURRENT_TIMESTAMP,
+    CHANGE COLUMN `updated_at` `updated_at` DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP;
+
+describe products;
+SELECT created_at from products;
+
+create table storehouses_products (
+	id SERIAL PRIMARY KEY,
+    storehouse_id INT unsigned,
+    product_id INT unsigned,
+    `value` INT unsigned COMMENT 'Запас товарный позиции на складке',
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) COMMENT = 'Запасы на складе';
+
+INSERT INTO
+    storehouses_products (storehouse_id, product_id, value)
+VALUES
+    (1, 1, 15),
+    (1, 3, 0),
+    (1, 5, 10),
+    (1, 7, 5),
+    (1, 8, 0);
+
+SELECT 
+    value
+FROM
+    storehouses_products ORDER BY CASE WHEN value = 0 then 1 else 0 end, value;
